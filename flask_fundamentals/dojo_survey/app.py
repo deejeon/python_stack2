ad6ba010b49886f4ff9e1fffa9726f71c333d8e6
Daniel Jeon <djeon93@gmail.com>
2024-06-24 11:08:37 -0700

    finished dojo survey

--- flask_fundamentals/dojo_survey/app.py
+++ flask_fundamentals/dojo_survey/app.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/result', methods=['POST'])
+def save_result():
+    dict = {}
+    dict['name'] = request.form['name']
+    dict['location'] = request.form['location']
+    dict['language'] = request.form['language']
+    dict['comment'] = request.form['comment']
+    return render_template('result.html', context=dict)
+
 if __name__ in "__main__":
     app.run(debug=True)
